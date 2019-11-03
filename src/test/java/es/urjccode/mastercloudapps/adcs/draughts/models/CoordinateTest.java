d8655b935a4013edbc0a499b575fcc2f893fc2f5
Pablo Calvo Sastre <>
2019-11-03 18:59:39 +0100

    change coordinate Test

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
@@ -3,21 +3,21 @@ package es.urjccode.mastercloudapps.adcs.draughts.models;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class CoordinateTest {
 
     @Test
     public void testGivenTwoCoordinatesWhenBettweenDiagonalThenOk() {
-        assertEquals(new Coordinate(1, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0)));
-        assertEquals(new Coordinate(3, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0)));
-        assertEquals(new Coordinate(3, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4)));
-        assertEquals(new Coordinate(1, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4)));
+        assertThat(new Coordinate(1, 1), is(new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0))));
+        assertThat(new Coordinate(3, 1), is(new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0))));
+        assertThat(new Coordinate(3, 3), is(new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4))));
+        assertThat(new Coordinate(1, 3), is(new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4))));
     }
 
     @Test
-    public void x(){
-        assertEquals(3,new Coordinate(3, 4).diagonalDistance(new Coordinate(0, 7)));
+    public void testGivenDiagonalBetwennTwhoCoordinate(){
+        assertThat(3, is(new Coordinate(3, 4).diagonalDistance(new Coordinate(0, 7))));
     }
 
 }
